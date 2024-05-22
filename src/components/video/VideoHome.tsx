b82bd7829a910202e987a756c29175f63f7367f3
venu reddy <venugopalreddy9493@gmail.com>
2024-05-22 10:01:06 +0530

    toggle switch added'

--- src/components/video/VideoHome.tsx
+++ src/components/video/VideoHome.tsx
@@ -6,10 +6,11 @@ import { AppDispatch, RootState } from '../../Redux/store';
 import { getAllCallsHistory, getAllLiveCalls } from '../../Redux/reducers/Calls/CallsReducer';
 import LiveCalls from './LiveCalls';
 import { useNavigate } from 'react-router-dom';
+import CreateCall from './CreateCall';
 
 const VideoHome = () => {
     const dispatch: AppDispatch = useDispatch()
-    const navigate= useNavigate()
+    const navigate = useNavigate()
     const { recentCalls, liveCalls } = useSelector((state: RootState) => state.calls)
     const { user } = useSelector((state: RootState) => state.auth)
 
@@ -33,8 +34,9 @@ const VideoHome = () => {
                 <aside className='col-span-3'>
                     <LiveCalls calls={liveCalls} />
                 </aside>
-                <main className='col-span-4 bg-black'>
+                <main className='col-span-4 '>
                     {/* <button className='w-full p-2 border mt-5 hover:shadow-xl transition-all hover:bg-yellow-100  ease-linear shadow-md rounded-sm' onClick={() => setContactInfo(!contactInfo)}>click me</button> */}
+                    <CreateCall />
                 </main>
                 <section className={`sm:col-span-3 sm:static fixed top-0 right-0 w-full transition-all ease-linear  duration-300 delay-150 ${contactInfo === true ? "-translate-x-0  z-20" : "sm:-translate-x-0 translate-x-full"}`}>
                     <CallHistory calls={recentCalls} />
