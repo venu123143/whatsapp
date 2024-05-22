ae6920c8474ac114ad74f98cd88a8bfeafc0ed8c
venu reddy <venugopalreddy9493@gmail.com>
2024-05-22 08:14:47 +0530

    live calls ui done

--- src/components/video/VideoHome.tsx
+++ src/components/video/VideoHome.tsx
@@ -3,11 +3,15 @@ import React, { useEffect, useState } from 'react'
 import CallHistory from './CallHistory';
 import { useDispatch, useSelector } from 'react-redux';
 import { AppDispatch, RootState } from '../../Redux/store';
-import { getAllCallsHistory } from '../../Redux/reducers/Calls/CallsReducer';
+import { getAllCallsHistory, getAllLiveCalls } from '../../Redux/reducers/Calls/CallsReducer';
+import LiveCalls from './LiveCalls';
+import { useNavigate } from 'react-router-dom';
 
 const VideoHome = () => {
     const dispatch: AppDispatch = useDispatch()
-    const { recentCalls } = useSelector((state: RootState) => state.calls)
+    const navigate= useNavigate()
+    const { recentCalls, liveCalls } = useSelector((state: RootState) => state.calls)
+    const { user } = useSelector((state: RootState) => state.auth)
 
     const [contactInfo, setContactInfo] = useState(false)
     // const location = useLocation();
@@ -15,14 +19,21 @@ const VideoHome = () => {
     // const { userId } = useParams()
     useEffect(() => {
         dispatch(getAllCallsHistory())
+        dispatch(getAllLiveCalls())
         setContactInfo(false)
     }, [])
+    useEffect(() => {
+        if (user === null) {
+            navigate('/login')
+        }
+    }, [user])
     return (
         <>
             <section className='block sm:grid grid-cols-10 w-full h-screen'>
-                <aside className='col-span-3 bg-red-500'>
+                <aside className='col-span-3'>
+                    <LiveCalls calls={liveCalls} />
                 </aside>
-                <main className='col-span-4 bg-blue-500'>
+                <main className='col-span-4 bg-black'>
                     {/* <button className='w-full p-2 border mt-5 hover:shadow-xl transition-all hover:bg-yellow-100  ease-linear shadow-md rounded-sm' onClick={() => setContactInfo(!contactInfo)}>click me</button> */}
                 </main>
                 <section className={`sm:col-span-3 sm:static fixed top-0 right-0 w-full transition-all ease-linear  duration-300 delay-150 ${contactInfo === true ? "-translate-x-0  z-20" : "sm:-translate-x-0 translate-x-full"}`}>
