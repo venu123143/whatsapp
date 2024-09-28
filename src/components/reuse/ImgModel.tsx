36d2dfea55c6cc12dd97557043bea1151bedfa13
venu reddy <venugopalreddy9493@gmail.com>
2024-09-29 00:43:33 +0530

    draggable added to img model

--- src/components/reuse/ImgModel.tsx
+++ src/components/reuse/ImgModel.tsx
@@ -1,10 +1,16 @@
-// src/components/ImageModal.tsx
-import React from 'react';
+
+import React, { useState, useEffect } from 'react';
 import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
-import { setCurrentImage, setIsFullscreen, setZoomLevel, setCurrentIndex } from '../../Redux/reducers/utils/Features';
-import { useDispatch, useSelector } from 'react-redux';
-import { AppDispatch, RootState } from '../../Redux/store';
 import { RxCross2 } from 'react-icons/rx';
+import { useDispatch, useSelector } from 'react-redux';
+import Draggable from 'react-draggable';
+import { AppDispatch, RootState } from '../../Redux/store';
+import {
+    setCurrentImage,
+    setIsFullscreen,
+    setZoomLevel,
+    setCurrentIndex
+} from '../../Redux/reducers/utils/Features';
 
 interface ImageModalProps {
     imageUrl: string;
@@ -12,51 +18,97 @@ interface ImageModalProps {
 }
 
 const ImageModal: React.FC<ImageModalProps> = ({ imageUrl, onClose }) => {
-    const { isFullscreen, currentIndex, images } = useSelector((state: RootState) => state.features)
-    const dispatch: AppDispatch = useDispatch()
+    const { isFullscreen, currentIndex, images } = useSelector((state: RootState) => state.features);
+    const dispatch: AppDispatch = useDispatch();
+    const [position, setPosition] = useState({ x: 0, y: 0 });
+
+    useEffect(() => {
+        const handleKeyDown = (e: KeyboardEvent) => {
+            if (isFullscreen) {
+                switch (e.key) {
+                    case 'ArrowLeft':
+                        prevImage();
+                        break;
+                    case 'ArrowRight':
+                        nextImage();
+                        break;
+                    case 'Escape':
+                        setPosition({ x: 0, y: 0 });
+                        onClose();
+                        break;
+                }
+            }
+        };
+
+        window.addEventListener('keydown', handleKeyDown);
+
+        return () => {
+            window.removeEventListener('keydown', handleKeyDown);
+        };
+    }, [isFullscreen, currentIndex, images]);
 
     const handleOverlayClick = (e: React.MouseEvent<HTMLDivElement>) => {
         if (e.target === e.currentTarget) {
+            setPosition({ x: 0, y: 0 });
             onClose();
         }
     };
+
     const nextImage = () => {
         if (currentIndex !== null && currentIndex < images.length - 1) {
-            dispatch(setZoomLevel(1))
-            dispatch(setCurrentIndex(currentIndex + 1))
-            dispatch(setCurrentImage(images[currentIndex + 1].file))
-
+            setPosition({ x: 0, y: 0 }); // Reset position when changing images
+            dispatch(setZoomLevel(1));
+            dispatch(setCurrentIndex(currentIndex + 1));
+            dispatch(setCurrentImage(images[currentIndex + 1].file));
         } else if (currentIndex! >= images.length - 1) {
-            dispatch(setIsFullscreen(false))
+            dispatch(setIsFullscreen(false));
         }
     };
 
     const prevImage = () => {
         if (currentIndex !== null && currentIndex > 0) {
-            dispatch(setZoomLevel(1))
-            dispatch(setCurrentIndex(currentIndex - 1))
-
-            // setCurrentImage(URL.createObjectURL(images[currentIndex - 1]));
-            dispatch(setCurrentImage(images[currentIndex - 1].file))
-            // setIsZoomed(false); // Reset zoom state when changing images
+            setPosition({ x: 0, y: 0 }); // Reset position when changing images
+            dispatch(setZoomLevel(1));
+            dispatch(setCurrentIndex(currentIndex - 1));
+            dispatch(setCurrentImage(images[currentIndex - 1].file));
         } else if (currentIndex! <= 0) {
             dispatch(setIsFullscreen(false));
         }
     };
+
+    const handleDrag = (_: any, ui: any) => {
+        const { x, y } = ui;
+        setPosition({ x, y });
+    };
+
     return (
-        <div className={`${isFullscreen ? "z-10 scale-100 fixed" : "z-0 scale-0"} transition-all  ease-in-out inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50`}
-            onClick={handleOverlayClick}>
-            <button className="prev-button  hover:bg-gray-600 rounded-full transform -translate-y-1/2 text-white text-2xl p-2" onClick={prevImage}>
-                <FaArrowLeft />
+        <div
+            className={`${isFullscreen ? "z-10 scale-100 fixed" : "z-0 scale-0"
+                } transition-all ease-in-out inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50`}
+            onClick={handleOverlayClick}
+        >
+            <button
+                className="prev-button hover:bg-gray-600 rounded-full transform -translate-y-1/2 text-white text-2xl p-2"
+                onClick={prevImage}
+            >
+                <FaArrowLeft title='left (left arrow)' />
             </button>
-            <div className=" max-w-3xl my-auto w-full mx-4">
-                <img src={imageUrl} alt="Selected" className="max-h-screen w-full object-contain" />
-            </div>
-            <button className="next-button  hover:bg-gray-600 rounded-full transform -translate-y-1/2 text-white text-2xl p-2" onClick={nextImage}>
-                <FaArrowRight />
+            <Draggable position={position} onDrag={handleDrag}>
+                <div className="max-w-3xl my-auto w-full mx-4 cursor-move">
+                    <img src={imageUrl} alt="Selected" className="max-h-screen w-full object-contain" />
+                </div>
+            </Draggable>
+            <button
+                className="next-button hover:bg-gray-600 rounded-full transform -translate-y-1/2 text-white text-2xl p-2"
+                onClick={nextImage}
+            >
+                <FaArrowRight title='right (right arrow)' />
             </button>
-            <button className="close-button  p-2 hover:bg-gray-600 rounded-full text-white text-2xl" onClick={onClose}>
-                <RxCross2 size={25} />
+            <button
+                className="close-button p-2 hover:bg-gray-600 rounded-full text-white text-2xl"
+                onClick={onClose}
+            >
+                <RxCross2 title='cancel (esc)' size={25} />
             </button>
         </div>
     );
